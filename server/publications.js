63766b3be1322353b320a8e492ad78fe0a1fb421
chris-hallquist <challquist@gmail.com>
2015-03-25 13:26:35 -0700

    Began work on deckStudy page

--- server/publications.js
+++ server/publications.js
@@ -2,6 +2,15 @@ Meteor.publish('decks', function() {
 	return Decks.find({userId: this.userId});
 })
 
-Meteor.publish('cards', function() {
+Meteor.publish('singleDeck', function(id) {
+	check(id, String);
+	return Decks.find(id);
+});
+
+Meteor.publish('cards', function(deckId) {
 	return Cards.find();
 })
+
+Meteor.publish('singleDeckCards', function(deckId){
+	return Cards.find({deckId: deckId});
+});
