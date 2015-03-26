e220dd090fcd40af46279a69b84bf0c8e05b190c
chris-hallquist <challquist@gmail.com>
2015-03-25 23:50:40 -0700

    Added button to show card back

--- client/templates/decks/deck_study.js
+++ client/templates/decks/deck_study.js
@@ -1,13 +1,35 @@
+Template.deckStudy.created = function() {
+	// We want to initially not show the back of the card
+}
+
 Template.deckStudy.helpers({
 	deck: function () {
 		return Decks.findOne();
 	},
 	card: function () {
-		return Cards.findOne({nextReview: {$lte: new Date()}});
+		return Cards.findOne({
+			nextReview: {$lte: new Date()}
+		});
 	},
 	front: function () {
 		return new Spacebars.SafeString(Cards.findOne({
 			nextReview: {$lte: new Date()}
 		}).front);
+	},
+	back: function () {
+		return new Spacebars.SafeString(Cards.findOne({
+			nextReview: {$lte: new Date()}
+		}).back);
+	},
+	showCardBack: function () {
+		return Session.get('showCardBack');
+	}
+});
+
+Template.deckStudy.events({
+	'click .showAnswer': function(e) {
+		e.preventDefault();
+		
+		Session.set('showCardBack', true);
 	}
 })
